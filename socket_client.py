45eca02d9b0522588154a1c54edba04a777693a8
ryanou97 <zhonglinou.1997@gmail.com>
2023-11-09 22:54:45 +0800

    update full

--- socket_client.py
+++ socket_client.py
@@ -5,19 +5,19 @@
 
 import socket
 
-# 创建 socket 对象
+# 創建 socket 對象
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# 获取本地主机名
+# 獲取本地主機名
 host = socket.gethostname()
 port = 12345
 
-# 连接服务，指定主机和端口
+# 連線服務，指定主機和端口
 client_socket.connect((host, port))
 
-# 接收欢迎消息
+# 接收消息
 message = client_socket.recv(1024)
 print(message.decode('utf-8'))
 
-# 关闭连接
+# 關閉連線
 client_socket.close()
--- socket_server.py
+++ socket_server.py
@@ -5,27 +5,27 @@
 
 import socket
 
-# 创建 socket 对象
+# 創建socket對象
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# 获取本地主机名
+# 獲取本地主機名
 host = socket.gethostname()
 port = 12345
 
 # 绑定端口
 server_socket.bind((host, port))
 
-# 设置最大连接数，超过后排队
+# 設置最大連接數
 server_socket.listen(5)
 
 while True:
-    # 建立客户端连接
+    # 建立客戶端連線
     client_socket, addr = server_socket.accept()
-    print('连接地址：', addr)
+    print('連線地址：', addr)
 
-    # 发送消息给客户端
-    message = '欢迎访问服务器！'
+    # 發送消息
+    message = 'welcome to server！'
     client_socket.send(message.encode('utf-8'))
 
-    # 关闭连接
+    # 關閉連線
     client_socket.close()
