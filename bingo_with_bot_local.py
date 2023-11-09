6a6aa4edfccbfdfead2302b54fa67a3047da65ce
ryanou97 <zhonglinou.1997@gmail.com>
2023-11-09 23:02:12 +0800

    update commit

--- bingo_with_bot_local.py
+++ bingo_with_bot_local.py
@@ -7,7 +7,7 @@ import random
 
 
 
-# 生成Bingo游戏卡片
+# 生成Bingo卡片
 def generate_bingo_card():
     
     numbers = list(range(1, 26))        # 生成1到25的数字列表
@@ -20,7 +20,7 @@ def generate_bingo_card():
 
 
 
-# 打印Bingo卡片
+# 顯示Bingo卡片
 def print_bingo_card(card):
     print(" B   I   N   G   O")
     
@@ -29,9 +29,9 @@ def print_bingo_card(card):
         print(" | ".join(f"{number:2d}" for number in row))
         print("-" * 25)
 
+ 
 
-
-# 检查Bingo卡片是否获胜
+# 檢查獲勝
 def check_bingo(card):
     
     for row in card:
@@ -52,36 +52,78 @@ def check_bingo(card):
     return False
 
 
+# 檢查這張牌能不能出
+# def play_card()
 
-# 主游戏循环
+
+# 主循環
 def play_bingo():
     
-    computer_card = generate_bingo_card()
+    # 能出的牌 
+    can_play_card = list(range(1, 26))
+   
+    # 亂數產生雙方的牌
     player_card = generate_bingo_card()
+    computer_card = generate_bingo_card()
     
     print("歡迎B來玩ingo遊戲！")
     print("您的Bingo卡片：")
     print_bingo_card(player_card)
+
     
     while True:
-        called_number = random.randint(1, 75)
-        print(f"叫号：{called_number}")
+        
+        
+        # 玩家出牌
+        player_called_number = int(input("Player call number: "))
+        can_play_card.remove(player_called_number)
         
         for row in player_card:
-            if called_number in row:
-                index = row.index(called_number)
+            if player_called_number in row:
+                index = row.index(player_called_number)
                 row[index] = 0
-        
+            
         print("您的Bingo卡片：")
         print_bingo_card(player_card)
+        
         
         if check_bingo(player_card):
             print("恭喜！您獲得了Bingo！")
             break
         
         for row in computer_card:
-            if called_number in row:
-                index = row.index(called_number)
+            if player_called_number in row:
+                index = row.index(player_called_number)
+                row[index] = 0
+        
+        if check_bingo(computer_card):
+            print("電腦獲勝！")
+            break
+        
+    
+        
+        # 電腦出牌
+        bot_called_number = random.choice(can_play_card)
+        can_play_card.remove(bot_called_number)
+        print(f"Bot call number: {bot_called_number}")
+        
+        
+        for row in player_card:
+            if bot_called_number in row:
+                index = row.index(bot_called_number)
+                row[index] = 0
+        
+        print("您的Bingo卡片：")
+        print_bingo_card(player_card)
+        
+        
+        if check_bingo(player_card):
+            print("恭喜！您獲得了Bingo！")
+            break
+        
+        for row in computer_card:
+            if bot_called_number in row:
+                index = row.index(bot_called_number)
                 row[index] = 0
         
         if check_bingo(computer_card):
@@ -90,8 +132,8 @@ def play_bingo():
 
 
 if __name__ == "__main__":
-    #play_bingo()
-    card = generate_bingo_card()
+    play_bingo()
+    #card = generate_bingo_card()
 
 
 
